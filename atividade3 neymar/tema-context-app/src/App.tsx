cb72f734f65d537b60b2a2f00b544897c75691fc
Felipe Ribeiro <felipe_ribeiro_286@hotmail.com>
2026-03-17 21:09:40 -0300

    Add files via upload

---
+++ atividade3 neymar/tema-context-app/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import{useTheme} from './context/ThemeContext';
+
+
+function App() {
+  const{ tema, alternarTema}= useTheme();
+
+  const estilo = {
+    backgroundColor: tema ==='claro'? '#fff' : '#333',
+    color: tema === 'claro' ? '#000' : '#fff',
+    height: '100vh',
+    padding: '2rem',
+    };
+
+
+  return (
+    <div style={estilo}>
+      <h1>Tema atual: {tema}</h1>
+      <button onClick={alternarTema}> Altenar Tema</button>
+      </div>
+  );
+}
+
+export default App;
